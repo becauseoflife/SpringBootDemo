5cc661f5cfad1a3cbd039d81c6dec85a15970bfc
becauseoflife <1752196851@qq.com>
2020-03-03 16:42:00 +0800

    登录服务

--- src/main/java/com/bookkeeping/contrller/WXLoginController.java
+++ src/main/java/com/bookkeeping/contrller/WXLoginController.java
@@ -1,18 +1,37 @@
 package com.bookkeeping.contrller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookkeeping.pojo.JSONResult;
+import com.bookkeeping.pojo.UserInfo;
+import com.bookkeeping.service.UserService;
 
 @RestController
 class WXLoginController {
 	
-	@PostMapping("/wxLogin")
-	public JSONResult wxLogin(String code) {
+	@Autowired
+	private UserService service;
+	
+	@PostMapping("/login")
+	public JSONResult wxLogin(String account, String password) {
 		
-		System.out.print("wxLogin code :" + code);
-		
-		return JSONResult.ok();
+		System.out.println("account:" + account + "  " + "password:" + password);
+		// 检车账号是否存在
+		UserInfo user = new UserInfo();
+		user = service.queryUserByAccount(account);
+		if(user == null) {
+			return JSONResult.errorMsg("账号不存在，请先注册");
+		}else {
+			// 检查密码是否正确
+			System.out.println("user password: " + user.getPassword());
+			if(!user.getPassword().equals(password)) {
+				return JSONResult.errorMsg("密码错误");
+			}else {
+				return JSONResult.ok("登录成功");
+			}
+		}
+
 	}
 }
